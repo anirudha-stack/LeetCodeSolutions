dd0e12cb334f5f5959d4633d306ab7fd98e806c6
Anirudha Gaikwad <anirudha.gaikwad@bajajfinserv.in>
2025-01-08 18:14:11 +0530

    Rotate Array started

---
+++ RotateArray/RotateArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] arrayToRotate = { 7, 1, 5, 3, 6, 4, 1, 2, 3, 4 };
+        Console.WriteLine("arrayToRotate: " + string.Join(", ", arrayToRotate));
+
+        int output = Rotate(arrayToRotate);
+
+        Console.WriteLine("Output: " + output);
+
+
+
+    }
+
+    public void Rotate(int[] nums, int k)
+    {
+
+        for(int i=0;i<nums.Length; i++)
+        {
+            //nums[]
+        }
+
+    }
+}
+
