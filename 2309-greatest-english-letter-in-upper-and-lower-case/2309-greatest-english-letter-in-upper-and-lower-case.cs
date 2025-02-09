0ad3741a90b3b2d019b05244ada49b4c6c573790
anirudha-stack <65551533+anirudha-stack@users.noreply.github.com>
2025-02-09 12:59:04 +0530

    Time: 329 ms (6.98%), Space: 42 MB (41.86%) - LeetHub

---
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public string GreatestLetter(string s) {
+        char greatest ='\0';
+       for(int i=0;i<s.Length;i++){
+        for(int j=i+1;j<s.Length;j++){
+            //Console.WriteLine($"Comparing {s[i]} and {s[j]}");
+                if((Char.ToLower(s[i]) == Char.ToLower(s[j])) && Math.Abs(s[j]-s[i])==32 && 
+                 Char.ToUpper(s[i]) > greatest
+                 ){
+                        greatest = Char.ToUpper(s[i]);
+                       // Console.WriteLine($"Greatest is  {s[j]}");
+            
+                }
+        }
+
+       }
+
+        GC.Collect();
+
+        if(greatest =='\0') return string.Empty;
+       return Convert.ToString(greatest);
+    }
+}
