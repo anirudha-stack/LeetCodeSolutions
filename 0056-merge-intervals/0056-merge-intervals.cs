f64d7531773099ac9996b109890949abc096f229
anirudha-stack <65551533+anirudha-stack@users.noreply.github.com>
2025-04-18 23:08:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cs
@@ -0,0 +1,43 @@
+public class Solution {
+    public int[][] Merge(int[][] intervals) {
+        Array.Sort(intervals, (a, b) => {
+            int cmp = a[0].CompareTo(b[0]);
+            return cmp != 0 ? cmp : a[1].CompareTo(b[1]);
+        });
+        int m = intervals.Length-1;
+        List<(int,int)> output = new List<(int,int)>();
+        int start =0;
+        int end = 0;
+        for(int i=0;i<=m;i++){
+            
+            int runner = i;
+            start = intervals[i][0];
+            end = intervals[i][1];
+          
+            if(output.Count != 0){
+                 var lastEntry = output[output.Count - 1];
+                 if(end <= lastEntry.Item2){
+                    continue;
+                 }
+            }
+
+            for( int j=i+1;j<m;j++){
+                if(intervals[j][0] <= end ){
+                    end = Math.Max(end,intervals[j][1]);
+                }
+                else{
+                    break;
+                }
+            }
+            //Console.WriteLine($"{start},{end}");
+            output.Add((start,end));
+           
+        }
+
+
+
+        return output
+    .Select(t => new int[] { t.Item1, t.Item2 })
+    .ToArray();
+    }
+}
