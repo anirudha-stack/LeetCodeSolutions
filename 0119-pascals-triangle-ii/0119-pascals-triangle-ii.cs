226067d5e0462abfefba760541ec6418eaac9f2e
anirudha-stack <65551533+anirudha-stack@users.noreply.github.com>
2025-04-27 15:18:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cs
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cs
@@ -1,14 +1,15 @@
 public class Solution {
     public IList<int> GetRow(int rowIndex) {
-         List<int> row = new List<int>();
-            long value = 1;
+         List<int> answer = new List<int>();
+         long value = 1;
 
-            for (int k = 0; k <= rowIndex; k++) {
-                row.Add((int)value);
-                value = value * (rowIndex - k) / (k + 1);
-            }
+         for(int i=0;i<=rowIndex;i++){
+            answer.Add(value);
 
-            return row;
+            value = value * (rowIndex-i)/(k+1);
+         }
+
+         return answer.ToArray();
     }
 
 
