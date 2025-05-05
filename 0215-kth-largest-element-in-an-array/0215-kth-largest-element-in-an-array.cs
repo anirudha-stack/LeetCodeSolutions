abeddc8e4451f5959c623634c1aef697dc51fafb
anirudha-stack <65551533+anirudha-stack@users.noreply.github.com>
2025-05-05 23:34:42 +0530

    Time: 166 ms (24.45%), Space: 57.4 MB (53.39%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cs
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cs
@@ -1,7 +1,15 @@
 public class Solution {
     public int FindKthLargest(int[] nums, int k) {
-        Array.Sort(nums);
+       var minHeap = new PriorityQueue<int,int>();
 
-        return nums[nums.Length-k];
+        foreach (var num in nums) {
+            minHeap.Enqueue(num, num);
+            if (minHeap.Count > k) {
+                minHeap.Dequeue();
+            }
+        }
+
+        // the root is now the k-th largest
+        return minHeap.Peek();
     }
 }
